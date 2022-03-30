470c509faa334bd603cd642695c5c17f0fad52d8
qinyu <chinyu0704@outlook.com>
2022-03-30 21:07:44 +0800

    add setup_repo.sh

---
+++ setup_repo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+find . -name "*.[chxsS]" -print > cscope.files
+
+#The -b flag tells Cscope to just build the database, and not launch the Cscope GUI. The -q causes an additional, 'inverted index' file to be created, which makes searches run much faster for large databases. Finally, -k sets Cscope's 'kernel' mode--it will not look in /usr/include for any header files that are #included in your source files (this is mainly useful when you are using Cscope with operating system and/or C library source code, as we are here).
+cscope -b -q -k
